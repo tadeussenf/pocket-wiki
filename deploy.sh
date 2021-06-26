41e261ec8850bb8f07ba8cdf3f27372e70dac242
Christoph Strauß <strauss@w11k.de>
2021-06-26 16:48:40 +0200

    add build step to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
+cd client && npm run build
 ssh tadeus@kojima.uberspace.de rm -rf /home/tadeus/html/pocket-wiki/*
-scp -r client/dist/* tadeus@kojima.uberspace.de:/home/tadeus/html/pocket-wiki
+scp -r dist/* tadeus@kojima.uberspace.de:/home/tadeus/html/pocket-wiki
